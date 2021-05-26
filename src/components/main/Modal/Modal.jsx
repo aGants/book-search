bb43af2564c139c64aa5e461a236dc696da46ecf
Adelia <adelgants@gmail.com>
2021-05-27 01:26:42 +0300

    add modal window

---
+++ src/components/main/Modal/Modal.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './modal.scss'
+
+const Modal = ({ active, setActive, props }) => {
+  
+  return (
+    <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>
+      <div className="modal-content" onClick={e => e.stopPropagation()}>
+      </div>
+    </div>
+  )
+}
+
+export default Modal;
--- src/components/main/book/Book.jsx
+++ src/components/main/book/Book.jsx
@@ -1,30 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './book.scss';
+import Modal from '../Modal/Modal';
 
 const Book = (props) => {
+  const [modalActive, setModalActive] = useState(false)
   const book = props.book
   
   return (
-    <div className="book">
-      {book.cover_i ? (
-        <img className="book__cover" src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`} alt={`Обложка ${book.title}`} />      
-      ) : (
-        <div className="book__cover-block">
-          <p>{ book.title }</p>
-          </div>
-      )
-      }
-      <div className="book-info">
-        <p className="book-info__title">{ book.title }</p>
-          <p className="book-info-authors">
-            {book.author_name ? (
-              book.author_name.map((author, index) => 
-              <span key={ index } className="book-info-authors__name">{ author }</span>
-              )) : (<span className="book-info-authors__name">Неизвестный автор</span>)
-            }
-          </p>
-      </div>
-      
+    <div className="book" >
+      <a onClick={() => setModalActive(true)}>
+        {book.cover_i ? (
+          <img className="book__cover" src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`} alt={`Обложка ${book.title}`} />      
+        ) : (
+          <div className="book__cover-block">
+            <p>{ book.title }</p>
+            </div>
+        )
+        }
+        <div className="book-info">
+          <p className="book-info__title">{ book.title }</p>
+            <p className="book-info-authors">
+              {book.author_name ? (
+                book.author_name.map((author, index) => 
+                <span key={ index } className="book-info-authors__name">{ author }</span>
+                )) : (<span className="book-info-authors__name">Неизвестный автор</span>)
+              }
+            </p>
+        </div>
+      </a>
+      <Modal active={modalActive} setActive={setModalActive}/>
     </div>
   )
 }
