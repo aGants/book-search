e99c54252462ffc7854bd1fb073ca87167ef9809
Adelia <adelgants@gmail.com>
2021-05-27 03:01:20 +0300

    style modal window

--- src/components/actions/book.js
+++ src/components/actions/book.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {setBook, setIsError, setIsFetching} from '../../reducers/bookReducer'
 
-export const getBook = (searchTitle = "") => {
+export const getBook = (searchTitle = "/") => {
   if (searchTitle == "") {
     searchTitle = "/"
   }
@@ -9,6 +9,7 @@ export const getBook = (searchTitle = "") => {
     try {
       dispatch(setIsFetching(true))
       const response = await axios.get(`https://openlibrary.org/search.json?title=${searchTitle}`)
+      console.log(searchTitle)
       dispatch(setBook(response.data))
     } catch(e) {
       dispatch(setIsError(true))
--- src/components/main/Modal/Modal.jsx
+++ src/components/main/Modal/Modal.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './modal.scss'
 
-const Modal = ({ active, setActive, props }) => {
+const Modal = ({ active, setActive, children }) => {
   
   return (
     <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>
       <div className="modal-content" onClick={e => e.stopPropagation()}>
+        { children }
       </div>
     </div>
   )
